c8ba002f60c5a1952872cb62b34a887926c32c05
Artem Malyshev <proofit404@gmail.com>
2019-12-25 21:38:39 +0300

    Setup azure pipelines.

---
+++ tests/helpers/helpers.py
@@ -0,0 +1,12 @@
+import configparser
+import textwrap
+
+
+def tox_info(var):
+    """Get variable value from all sections in the tox.ini file."""
+    ini_parser = configparser.ConfigParser()
+    ini_parser.read("tox.ini")
+    for section in ini_parser:
+        if var in ini_parser[section]:
+            value = textwrap.dedent(ini_parser[section][var].strip())
+            yield section, value
